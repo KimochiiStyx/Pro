15589bb2dc7c3ffa64ca95293dce1ffe26cce9a6
KimochiiStyx <598053856@qq.com>
2017-03-22 13:53:37 +0000

    comm module added

---
+++ comm.c
@@ -0,0 +1,30 @@
+#include "comm.h"
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+
+void send_data(double leq[])
+{
+	char temp[30], postdata[100]="data=";  //post filed name is given
+	CURL *curl;
+	CURLcode res;
+	int i;  // prepare the post data
+	for(i=0;i<8;i++)
+	{
+		sprintf(temp, "%2.2f ", leq[i]);
+		strcat(postdata, temp);  //end of for loop
+	}
+	curl_global_init(CURL_GLOBAL_ALL);
+	curl=curl_easy_init();
+	if(curl)
+	{
+		curl_easy_setopt(curl, CURLOPT_URL, URL);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postdata);
+		res=curl_easy_perform(curl);
+		if(res!=CURLE_OK)
+			fprintf(stderr, "curl_easy_perform failed: %s\n",
+					curl_easy_strerror(res));
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+}
